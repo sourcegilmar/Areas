9799e43076d1e53e3a044264ed80570910553671
sourcegilmar <gilmardw@gmail.com>
2021-03-27 20:30:47 -0300

    2a refatoracao - extracao metodos areas

--- src/aplicacao/Areas.java
+++ src/aplicacao/Areas.java
@@ -22,35 +22,19 @@ public class Areas {
 				System.out.println("Opção inválida!");
 			switch (op) {
 			case 1:
-				System.out.println("Informe o lado: ");
-				double lado = sc.nextDouble();
-				area = Math.pow(lado, 2);
+				areaQuadrado(sc);
 				imprimeResultado("quadrado");
 				break;
 			case 2:				
-				System.out.println("Informe o primeiro lado do retângulo: ");
-				double lado1 = sc.nextDouble();
-				System.out.println("Informe o segundo lado do retângulo: ");
-				double lado2 = sc.nextDouble();
-				area = lado1 * lado2;
+				areaRetangulo(sc);
 				imprimeResultado("retângulo");			
 				break;
 			case 3:				
-				System.out.println("Informe a base do triângulo: ");
-				double base = sc.nextDouble();
-				System.out.println("Informe a altura do triângulo: ");
-				double altura = sc.nextDouble();
-				area = base * altura;
+				areaTriangulo(sc);
 				imprimeResultado("triângulo");
 				break;
 			case 4:
-				System.out.println("Informe a base Maior: ");
-				double baseMaior = sc.nextDouble();
-				System.out.println("Informe a base Menor: ");
-				double baseMenor = sc.nextDouble();
-				System.out.println("Informe a altura: ");
-				double alturaTrapezio = sc.nextDouble();
-				area = ((baseMaior + baseMenor)*alturaTrapezio)/2;
+				areaTrapezio(sc);
 				imprimeResultado("trapézio");
 				break;			
 			}
@@ -59,6 +43,34 @@ public class Areas {
 		System.out.println("* PROGRAMA FINALIZADO *");
 		System.out.println("-----------------------");
 
+	}
+	public static void areaTrapezio(Scanner sc) {
+		System.out.println("Informe a base Maior: ");
+		double baseMaior = sc.nextDouble();
+		System.out.println("Informe a base Menor: ");
+		double baseMenor = sc.nextDouble();
+		System.out.println("Informe a altura: ");
+		double alturaTrapezio = sc.nextDouble();
+		area = ((baseMaior + baseMenor)*alturaTrapezio)/2;
+	}
+	public static void areaTriangulo(Scanner sc) {
+		System.out.println("Informe a base do triângulo: ");
+		double base = sc.nextDouble();
+		System.out.println("Informe a altura do triângulo: ");
+		double altura = sc.nextDouble();
+		area = base * altura;
+	}
+	public static void areaRetangulo(Scanner sc) {
+		System.out.println("Informe o primeiro lado do retângulo: ");
+		double lado1 = sc.nextDouble();
+		System.out.println("Informe o segundo lado do retângulo: ");
+		double lado2 = sc.nextDouble();
+		area = lado1 * lado2;
+	}
+	public static void areaQuadrado(Scanner sc) {
+		System.out.println("Informe o lado: ");
+		double lado = sc.nextDouble();
+		area = Math.pow(lado, 2);
 	}
 	public static void imprimeResultado(String texto) {
 		System.out.println("-------------------------");
